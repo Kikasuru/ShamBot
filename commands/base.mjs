06b26320644a37720d801ea5d79f788b0b4cd526
Kikasuru <tgvaporeon@gmail.com>
2021-08-16 23:53:00 -0500

    Fixed some base commands

--- commands/base.mjs
+++ commands/base.mjs
@@ -5,8 +5,7 @@ export class PingCmd extends Command {
     static desc = "Replies with \"Pong!\"  Perfect for testing if I work!"
 
     static onRun(inter) {
-        const shamburger = inter.client.emojis.cache.get("876792351404752936");
-        inter.reply(`Pong! ${shamburger}`);
+        inter.reply(`Pong!`);
     }
 }
 
@@ -15,6 +14,7 @@ export class InviteCmd extends Command {
     static desc = "Replies with my invite!  Take me somewhere new!"
 
     static onRun(inter) {
-        inter.reply(`Thank you for showing intrest!  Here's my invite!\nhttps://discord.com/api/oauth2/authorize?client_id=${inter.client.user.id}&permissions=2147485760&scope=bot%20applications.commands`)
+        inter.reply(`Thank you for showing intrest!  Here's my invite!
+https://discord.com/api/oauth2/authorize?client_id=${inter.client.user.id}&permissions=2147485760&scope=bot%20applications.commands`)
     }
 }
